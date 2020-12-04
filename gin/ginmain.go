d452a8a076048fe7c306dd114113373dfcf16458
manlan <2733284198@qq.com>
2020-12-04 22:48:02 +0800

    gin: json 返回struct,

--- gin/ginmain.go
+++ gin/ginmain.go
@@ -4,13 +4,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Id   int64
+	Name string
+	Age  int
+}
+
+func init() {
+
+}
+
 func main() {
+	user := User{
+		Id:   1,
+		Name: "manlan1",
+		Age:  20,
+	}
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		//c.String(200, "Hello World")
 		c.JSON(200, gin.H{
-			"mess": "Hello World",
+			//"mess": "Hello World",
+			"user": user,
 		})
 	})
 
